28c5245fe29fc4e63e0b7444d1ce250d3ab928b1
its-mohd-kaif <mohdkaif@cedcoss.com>
2022-09-14 15:56:03 +0530

    Write a class to declare all the the details of the employee as mentioned in the above HTML page. Create an object of the class and display it.

---
+++ src/task2.js
@@ -0,0 +1,20 @@
+class employee {
+  constructor(id, name, salary) {
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+  }
+}
+
+let data = new employee(1, "John", 8000);
+var table = "<table border=1px><tr><th>ID</th><th>Name</th><th>Salary</th><tr>";
+table +=
+  "<tr><td>" +
+  data.id +
+  "</td><td>" +
+  data.name +
+  "</td><td>" +
+  data.salary +
+  "</td><tr>";
+table += "</table>";
+document.getElementById("output").innerHTML = table;
