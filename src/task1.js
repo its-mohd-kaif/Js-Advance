84f53985a0b3d1f94094e0ef77f617f0833ebce1
its-mohd-kaif <mohdkaif@cedcoss.com>
2022-09-14 15:27:26 +0530

    Design an HTML page to take the employee details as input from the user and display them according to the given format attached.

---
+++ src/task1.js
@@ -0,0 +1,45 @@
+let data = [];
+function add() {
+  let id = document.getElementById("id").value;
+  let name = document.getElementById("name").value;
+  let salary = document.getElementById("salary").value;
+
+  var val = {
+    ID: id,
+    Name: name,
+    Salary: salary,
+  };
+  data.push(val);
+
+  display();
+}
+
+function display() {
+  var table =
+    "<table><tr><th>ID</th><th>Name</th><th>Salary</th><th>Action</th>";
+  data.forEach((element) => {
+    table +=
+      "<tr><td>" +
+      element.ID +
+      "</td><td>" +
+      element.Name +
+      "</td><td>" +
+      element.Salary +
+      "</td><td>" +
+      "<button onclick=delFun('" +
+      element.ID +
+      "')>Delete</button>" +
+      "</td><tr>";
+  });
+  table += "</table>";
+  document.getElementById("output").innerHTML = table;
+}
+
+function delFun(val) {
+  for (let i = 0; i < data.length; i++) {
+    if (val == data[i].ID) {
+      data.splice(i, 1);
+    }
+  }
+  display();
+}
