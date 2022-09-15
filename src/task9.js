039d309394440e642b5d0097bebf693a9c202439
its-mohd-kaif <mohdkaif@cedcoss.com>
2022-09-15 10:08:14 +0530

    Apply employee name validation (not be empty, not a number). Use try, catch and throw and display the proper error message if any during validation.

--- src/task9.js
+++ src/task9.js
@@ -4,19 +4,23 @@ function add() {
   let name = document.getElementById("name").value;
   let salary = document.getElementById("salary").value;
 
-  var val = {
-    ID: id,
-    Name: name,
-    Salary: salary,
-  };
-  data.push(val);
-
-  display();
+  try {
+    if (name == "") throw "All field must be field ";
+    if (!isNaN(name)) throw "Name should not be a value";
+    var val = {
+      ID: id,
+      Name: name,
+      Salary: salary,
+    };
+    data.push(val);
+    display();
+  } catch (err) {
+    document.getElementById("err").innerHTML = err;
+  }
 }
 
 function display() {
-  var table =
-    "<table><tr><th>ID</th><th>Name</th><th>Salary</th></tr>";
+  var table = "<table><tr><th>ID</th><th>Name</th><th>Salary</th></tr>";
   data.forEach((element) => {
     table +=
       "<tr><td>" +
